152302a77654b8d15492546045703b95fc9831d7
Prasenjit Singha Roy <prasenjitsingharoy17@gmail.com>
2022-11-06 23:00:13 +0530

    added sub functionality

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ const addTwoNumbers = (a, b) => {
     return a+b;
 }
 
-const result = addTwoNumbers(1,2);
-console.log('1 + 2 = ' + result);
+const subTwoNumbers = (a, b) => {
+    return a-b;
+}
+
+const addResult = addTwoNumbers(1,2);
+const subResult = subTwoNumbers(2,1);
+console.log('1 + 2 = ' + addResult);
+console.log('2 - 1 = ' + subResult);
